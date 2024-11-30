2a461407bf4de4d1fa293a23a24949eb7e8b3456
Gerardo Martinez <guegueger@ciencias.unam.mx>
2024-11-30 01:41:37 -0600

    chores: upgrade readme and setup

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 Este repositorio contiene...
 
 La versión de Python que estoy utilizando es la 3.11.5
+Para instalar los modulos necesarios de modules desde la carpeta madre colocar pip install -e
 ## Estructura del Repositorio
 
 La estructura del repositorio está organizada en varias carpetas para facilitar la modularización y reutilización del código. Aquí se describe el contenido de cada carpeta:
--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="keet_database",  # Nombre de tu paquete
-    version="0.1",  # Versión inicial del paquete
-    packages=find_packages(where='modules'),  # Buscar paquetes en la carpeta modules
+    name="keet_database",  
+    version="0.1",  
+    packages=find_packages(where='modules'),  
     author="Hector Gerardo Martinez Fuentes",
     author_email="guegueger@gmail.com",
 )
