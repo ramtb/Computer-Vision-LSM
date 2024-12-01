5e6af4a5af321c5991c5954b7b57ee2dd5dcd7a7
Gerardo Martinez <guegueger@ciencias.unam.mx>
2024-12-01 01:39:30 -0600

    chore: added test_modules.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="keet_database",  
-    version="0.1",  
-    packages=find_packages(where='modules'),  
-    author="Hector Gerardo Martinez Fuentes",
-    author_email="guegueger@gmail.com",
-)
---
+++ test/test_modules.py
@@ -0,0 +1,6 @@
+from modules.basic_voice_system import *
+from modules.keet_database import *
+
+
+if __name__ == '__main__':
+    print('libreries successfully imported')
