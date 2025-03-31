e57d485581610298346a49adda5ba4907d315c44
Gerardo Martinez <guegueger@ciencias.unam.mx>
2025-03-31 01:20:52 -0600

    feat: gui_español and main merge

--- GUI/main.py
+++ GUI/main.py
@@ -17,7 +17,7 @@ from modules.faces.face_positions import FaceMeshDetector
 from modules.config_camera import CameraHandler
 
 from PySide6.QtWidgets import QApplication
-from gui_español import GUI 
+from gui_español import *
 
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
@@ -62,21 +62,23 @@ with open(style_path, "r") as qss_file:
 
 app.setStyleSheet(qss_style)
 
-gui = GUI(parent=None)
+gui = MainWindow(app)
 gui.show()
 
-def close_application():
-    global app_running
-    app_running = False
-    camera.release_camera()
-    cv2.destroyAllWindows()
-    app.quit()
+# def close_application():
+#     global app_running
+#     app_running = False
+#     camera.release_camera()
+#     cv2.destroyAllWindows()
+#     app.quit()
 
-gui.close_application.connect(close_application)
+
+# gui.close_application.connect(close_application)
 
 #########* CAMERA SETTINGS ###########
 
-camera = CameraHandler(camera_index=1, width_screen=1280, height_screen=720) ### 0 is the default camera, 1 is the external camera
+
+camera = CameraHandler(camera_index=0, width_screen=1280, height_screen=720) ### 0 is the default camera, 1 is the external camera
 
 camera.set_resolution(camera.width_screen, camera.height_screen) ### Set the resolution of the window of the frame
 width, height = camera.get_resolution() ### Get the resolution of the camera
@@ -114,125 +116,127 @@ app_running = True
 
 while app_running:
     app.processEvents()
-    
-    current_time = time.time()
-
-    ret, frame, frame_copy, frame_gray, frame_equali, results = tr.read_frames(camera,hands,equali=False)
-    
-    #######* HAND EXTRACTION ########
-    roi_save, save_len, point_save, lm_x_h1, lm_y_h1, lm_x_h2, lm_y_h2, lm_x_h1_roi, lm_y_h1_roi, lm_x_h2_roi, lm_y_h2_roi, flag = tr.process_hand_landmarks(frame_equali= frame_equali,
-                                                    results= results, width= width, height= height, t= t,tiempo_de_espera= 3
-                                                    ,save_len = None,print_lm=False, size_roi = 0.087, point_save={})       
-    
-    gui.update_led_status(flag == 1)
-    ################* FACE MESH ############	
-    face_mesh_results = face_mesh_images.process(frame)
-    
-    if face_mesh_results.multi_face_landmarks:
-        lm = face_mesh_results.multi_face_landmarks[0] 
-        landmarks = lm.landmark
-        # Extraer las posiciones de los landmarks
-        positions_x = np.array([landmark.x for landmark in landmarks])
-        positions_y = np.array([landmark.y for landmark in landmarks])
-
-        # Calcular el rectángulo alrededor de la cara
-        min_x, min_y = np.min(positions_x), np.min(positions_y)
-        max_x, max_y = np.max(positions_x), np.max(positions_y)
-
-        # Dibujar el rectángulo alrededor de la cara
-        cv2.rectangle(frame, (int(min_x * width), int(min_y * height)),
-                      (int(max_x * width), int(max_y * height)), (0, 0, 255), 3)
+    while gui.show_gui == True:
         
-        flag_face = 1
-    else:
-        flag_face = 0 
+        
+        current_time = time.time()
 
-    #############* REAL TIME MODEL FACE #########
-    if current_time - start_time >= delay_time:  
-        if flag_face == 1:
-            positions_x = (positions_x*width).astype(int)
-            positions_y = (positions_y*height).astype(int)
-            roi_positions_x = positions_x*((max_x-min_x)).astype(int)
-            roi_positions_y = positions_y*((max_y-min_y)).astype(int)
-            data =  np.hstack((positions_x, positions_y, roi_positions_x, roi_positions_y))
-            data = data.reshape(1, data.shape[0])
-            data_face = data.astype(int)
-            data_normalized_face = scaler_faces.transform(data_face)
-            predictions_face = model_faces.predict(data_normalized_face, verbose=0)
-            predicted_class_face = np.argmax(predictions_face, axis=1)
-            predictions_face = dict_labels_faces[predicted_class_face[0]]
-            predicted_face = True  # Se realizó una predicción
-            emotion_emoji = emotion_emojis.get(predictions_face, '')
-        # start_time = current_time
-    # Mostrar la predicción si se ha realizado
-    if predicted_face:
-        cv2.putText(frame, predictions_face, (20, 150), cv2.FONT_HERSHEY_SIMPLEX, 2, (50, 50, 200), 2)
-    
-    #############* REAL TIME MODEL HAND #########
-    if current_time - start_time >= delay_time:  
-        if flag == 1:
+        ret, frame, frame_copy, frame_gray, frame_equali, results = tr.read_frames(camera,hands,equali=False)
+        
+        #######* HAND EXTRACTION ########
+        roi_save, save_len, point_save, lm_x_h1, lm_y_h1, lm_x_h2, lm_y_h2, lm_x_h1_roi, lm_y_h1_roi, lm_x_h2_roi, lm_y_h2_roi, flag = tr.process_hand_landmarks(frame_equali= frame_equali,
+                                                        results= results, width= width, height= height, t= t,tiempo_de_espera= 3
+                                                        ,save_len = None,print_lm=False, size_roi = 0.087, point_save={})       
+        
+        gui.update_led_status(flag == 1)
+        ################* FACE MESH ############	
+        face_mesh_results = face_mesh_images.process(frame)
+        
+        if face_mesh_results.multi_face_landmarks:
+            lm = face_mesh_results.multi_face_landmarks[0] 
+            landmarks = lm.landmark
+            # Extraer las posiciones de los landmarks
+            positions_x = np.array([landmark.x for landmark in landmarks])
+            positions_y = np.array([landmark.y for landmark in landmarks])
+
+            # Calcular el rectángulo alrededor de la cara
+            min_x, min_y = np.min(positions_x), np.min(positions_y)
+            max_x, max_y = np.max(positions_x), np.max(positions_y)
+
+            # Dibujar el rectángulo alrededor de la cara
+            cv2.rectangle(frame, (int(min_x * width), int(min_y * height)),
+                        (int(max_x * width), int(max_y * height)), (0, 0, 255), 3)
             
-            if n_letters == 0:
-                phrase = ''
-            n_letters += 1
-            data =  [ np.reshape(np.array(list(lm_x_h1.values())),(21,1))
-                     , np.reshape(np.array(list(lm_x_h1_roi.values())),(21,1)), 
-                      np.reshape(np.array(list(lm_y_h1.values())),(21,1)), 
-                      np.reshape(np.array(list(lm_y_h1_roi.values())),(21,1)),]      
-            data = np.concatenate(data,axis=1)
-            data = data.reshape(1, 84)
-            data_normalized = scaler.transform(data)
-            predictions = model.predict(data_normalized, verbose=1)
-            predicted_class = np.argmax(predictions, axis=1)
-            # print(f'Predicción: {dict_labels[predicted_class[0]]}') 
-            prediction = dict_labels[predicted_class[0]]
-            predicted = True
-            phrase = phrase + prediction
-
-            # Add emoji
-            if predicted_face:
-                phrase_with_emoji = f"{emotion_emoji} {phrase}"
-            else:
-                phrase_with_emoji = phrase
-
-            gui.update_text(phrase_with_emoji) # Actualizar el texto
+            flag_face = 1
         else:
-            if n_letters > 0:
-                bvs.sintetizar_emocion('emocion=alegria', texto = phrase )
-                completed_translations.append(phrase)  # Añadir la traducción completada a la lista
-                gui.update_text('Esperando ...', completed_translations[-1])  # Ac
-                predicted = False
-                n_letters = 0
-        start_time = current_time    
-    if predicted == True and point_save != {}:
-        cv2.putText(frame, prediction, (point_save['h1_x_max'], point_save['h1_y_min']-40), cv2.FONT_HERSHEY_SIMPLEX, 2, (50, 50, 200), 3)
-        cv2.putText(frame, phrase , (point_save['h1_x_max'], point_save['h1_y_min']+50), cv2.FONT_HERSHEY_SIMPLEX, 2, (50, 50, 200), 3)
+            flag_face = 0 
+
+        #############* REAL TIME MODEL FACE #########
+        if current_time - start_time >= delay_time:  
+            if flag_face == 1:
+                positions_x = (positions_x*width).astype(int)
+                positions_y = (positions_y*height).astype(int)
+                roi_positions_x = positions_x*((max_x-min_x)).astype(int)
+                roi_positions_y = positions_y*((max_y-min_y)).astype(int)
+                data =  np.hstack((positions_x, positions_y, roi_positions_x, roi_positions_y))
+                data = data.reshape(1, data.shape[0])
+                data_face = data.astype(int)
+                data_normalized_face = scaler_faces.transform(data_face)
+                predictions_face = model_faces.predict(data_normalized_face, verbose=0)
+                predicted_class_face = np.argmax(predictions_face, axis=1)
+                predictions_face = dict_labels_faces[predicted_class_face[0]]
+                predicted_face = True  # Se realizó una predicción
+                emotion_emoji = emotion_emojis.get(predictions_face, '')
+            # start_time = current_time
+        # Mostrar la predicción si se ha realizado
+        if predicted_face:
+            cv2.putText(frame, predictions_face, (20, 150), cv2.FONT_HERSHEY_SIMPLEX, 2, (50, 50, 200), 2)
+        
+        #############* REAL TIME MODEL HAND #########
+        if current_time - start_time >= delay_time:  
+            if flag == 1:
                 
-           
-    
-    #*######### ENDS IF ##############
+                if n_letters == 0:
+                    phrase = ''
+                n_letters += 1
+                data =  [ np.reshape(np.array(list(lm_x_h1.values())),(21,1))
+                        , np.reshape(np.array(list(lm_x_h1_roi.values())),(21,1)), 
+                        np.reshape(np.array(list(lm_y_h1.values())),(21,1)), 
+                        np.reshape(np.array(list(lm_y_h1_roi.values())),(21,1)),]      
+                data = np.concatenate(data,axis=1)
+                data = data.reshape(1, 84)
+                data_normalized = scaler.transform(data)
+                predictions = model.predict(data_normalized, verbose=1)
+                predicted_class = np.argmax(predictions, axis=1)
+                # print(f'Predicción: {dict_labels[predicted_class[0]]}') 
+                prediction = dict_labels[predicted_class[0]]
+                predicted = True
+                phrase = phrase + prediction
+
+                # Add emoji
+                if predicted_face:
+                    phrase_with_emoji = f"{emotion_emoji} {phrase}"
+                else:
+                    phrase_with_emoji = phrase
+
+                gui.update_text(phrase_with_emoji) # Actualizar el texto
+            else:
+                if n_letters > 0:
+                    bvs.sintetizar_emocion('emocion=alegria', texto = phrase )
+                    completed_translations.append(phrase)  # Añadir la traducción completada a la lista
+                    gui.update_text('Esperando ...', completed_translations[-1])  # Ac
+                    predicted = False
+                    n_letters = 0
+            start_time = current_time    
+        if predicted == True and point_save != {}:
+            cv2.putText(frame, prediction, (point_save['h1_x_max'], point_save['h1_y_min']-40), cv2.FONT_HERSHEY_SIMPLEX, 2, (50, 50, 200), 3)
+            cv2.putText(frame, phrase , (point_save['h1_x_max'], point_save['h1_y_min']+50), cv2.FONT_HERSHEY_SIMPLEX, 2, (50, 50, 200), 3)
+                    
+            
         
-    cTime, fps, Ts, pTime, time_frames = tr.ends_if(cTime, fps, Ts, pTime, time_frames)
-        
+        #*######### ENDS IF ##############
+            
+        cTime, fps, Ts, pTime, time_frames = tr.ends_if(cTime, fps, Ts, pTime, time_frames)
+            
 
-    ################* DRAW RECTANGULOS and text ###############
+        ################* DRAW RECTANGULOS and text ###############
 
-    tr.draw_text_and_rectangles(point_save, frame, width, height, fps, draw_rectangules=True,draw_text=True)
+        tr.draw_text_and_rectangles(point_save, frame, width, height, fps, draw_rectangules=True,draw_text=True)
 
-    ##############* SHOW THE FRAMES #############
+        ##############* SHOW THE FRAMES #############
 
-    SAVED = sh.main_show(frame = frame, SAVED = None, width= width, height=height, roi_save= roi_save, window_move= window_move, df = None, RECORDING = None, t1 = None, save_len = None)
-        
-    #####* RESET THE LIST ########
+        SAVED = sh.main_show(frame = frame, SAVED = None, width= width, height=height, roi_save= roi_save, window_move= window_move, df = None, RECORDING = None, t1 = None, save_len = None)
+            
+        #####* RESET THE LIST ########
 
-    roi_save, point_save= res.reset_save(roi_save)
+        roi_save, point_save= res.reset_save(roi_save)
 
-    ###* EXIT
-        
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        app_running = False
+        ###* EXIT
+            
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            app_running = False
 
-close_application()
+    # close_application()
 
 for translation in completed_translations:
     print(f"Traducción completada: {translation}")
